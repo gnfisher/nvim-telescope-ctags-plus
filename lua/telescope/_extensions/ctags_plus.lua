a45cf5059bc0c282c272e368251eca9b12818548
Greg Fisher <greg@gnfisher.com>
2022-09-18 13:39:31 -0400

    Rename and reorder folder structure

--- lua/telescope/_extensions/ctags_plus.lua
+++ lua/telescope/_extensions/ctags_plus.lua
@@ -3,6 +3,6 @@ return require("telescope").register_extension {
     -- access extension config and user config
   end,
   exports = {
-    jump_to_tag = require("nvim-telescope-ctags-plus").jump_to_tag
+    jump_to_tag = require("nvim_telescope_ctags_plus").jump_to_tag
   },
 }
